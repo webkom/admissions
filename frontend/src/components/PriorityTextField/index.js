9788136b97d3485ba1befc6ccd1f108f5a58c165
Jonas Bakken <jonasbak@stud.ntnu.no>
2018-08-23 19:38:06 +0200

    Update sessionStorage on interval instead of on blur

--- frontend/src/components/PriorityTextField/index.js
+++ frontend/src/components/PriorityTextField/index.js
@@ -1,39 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import Textarea from "react-textarea-autosize";
 
-import { Card, CardParagraph, CardTitle } from "src/components/Card";
+import { Card, CardTitle } from "src/components/Card";
 
 import Wrapper from "./Wrapper";
 import "./style.css";
 
-const PriorityTextField = ({
-  field: { name, onChange, value },
-  form: { handleBlur }
-}) => {
-  return (
-    <Wrapper>
-      <Card className="input" margin="0.5rem 1rem">
-        <CardTitle margin="0.5rem" fontSize="0.8em">
-          Her kan du rangere komiteer etter ønske, og komme med andre
-          kommentarer.
-        </CardTitle>
-        <Textarea
-          className="textarea"
-          type="textarea"
-          name={name}
-          id={name}
-          onChange={onChange}
-          onBlur={e => {
-            handleBlur(e);
-            sessionStorage.setItem("text", value);
-          }}
-          placeholder="Skriv dine kommentarer her..."
-          value={value}
-          rows="10"
-        />
-      </Card>
-    </Wrapper>
-  );
-};
+class PriorityTextField extends Component {
+  componentDidMount() {
+    this.setState({
+      timeout: setInterval(() => {
+        sessionStorage.setItem("text", this.props.field.value);
+      }, 4000)
+    });
+  }
+  componentWillUnmount() {
+    clearInterval(this.state.timeout);
+  }
+  render() {
+    const {
+      field: { name, onChange, value },
+      form: { handleBlur }
+    } = this.props;
+    return (
+      <Wrapper>
+        <Card className="input" margin="0.5rem 1rem">
+          <CardTitle margin="0.5rem" fontSize="0.8em">
+            Her kan du rangere komiteer etter ønske, og komme med andre
+            kommentarer.
+          </CardTitle>
+          <Textarea
+            className="textarea"
+            type="textarea"
+            name={name}
+            id={name}
+            onChange={onChange}
+            onBlur={handleBlur}
+            placeholder="Skriv dine kommentarer her..."
+            value={value}
+            rows="10"
+          />
+        </Card>
+      </Wrapper>
+    );
+  }
+}
 
 export default PriorityTextField;
