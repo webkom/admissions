5e44f550680660e277838dd54fe38b24174fdd54
Sigrid Marita Kvamme <sigridkvamme@gmail.com>
2019-03-17 15:09:48 +0100

    Add compile_requirements command to simplify adding/updating deps

---
+++ committee_admissions/utils/management/commands/compile_requirements.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+import subprocess
+import os
+
+
+class Command(BaseCommand):
+
+    help = 'pip-compiles all requirements'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        FNULL = open(os.devnull, 'w')
+
+        subprocess.call(["pip-compile", "requirements/development.in"], stdout=FNULL)
+        subprocess.call(["pip-compile", "requirements/production.in"], stdout=FNULL)
