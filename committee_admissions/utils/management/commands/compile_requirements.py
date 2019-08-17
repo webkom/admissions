c0e1e98a54cb0f88c7930ba22f7c1fe268f239bc
Sigrid Marita Kvamme <sigridkvamme@gmail.com>
2019-08-18 00:40:44 +0200

    Add example env file. Add command for compiling requirements
    * Fixes #176

---
+++ committee_admissions/utils/management/commands/compile_requirements.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    help = "pip-compiles all requirements"
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        FNULL = open(os.devnull, "w")
+
+        subprocess.call(["pip-compile", "requirements/development.in"], stdout=FNULL)
+        subprocess.call(["pip-compile", "requirements/production.in"], stdout=FNULL)
