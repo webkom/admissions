0110a36863f63c6c864f30c606a183cff4f84e01
Jonatan Norbye <jonatan@norbye.com>
2022-07-25 15:49:09 +0200

    Bump django to 4.0.6 and postgres to 14.4
    * Bump deps that required django > 2.2.28
    * Set default_auto_field for models to satisfy django warnings
    * Removed deprecated django.conf.urls.url, replaced with re_path

--- committee_admissions/admissions/apps.py
+++ committee_admissions/admissions/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class AdmissionsConfig(AppConfig):
-    name = "admissions"
+    default_auto_field = "django.db.models.AutoField"
+    name = "committee_admissions.admissions"
