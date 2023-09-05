c0fec2d58916b3978935e3a6176a6119f904e1d5
PeterJFB <peterj.flbj@gmail.com>
2023-09-05 14:50:42 +0200

    Update superuser groups

--- admissions/admissions/constants.py
+++ admissions/admissions/constants.py
@@ -53,7 +53,7 @@ KOMTEK_LONG = "Kommunikasjonsteknologi"
 COURSES_LONG = ((DATA_LONG, DATA_LONG), (KOMTEK_LONG, KOMTEK_LONG))
 
 # Groups that give privileges to their leaders
-SUPERUSER_LEADER_GROUPS = ["Hovedstyret"]
+SUPERUSER_LEADER_GROUPS = ["Hovedstyret", "RevyStyret"]
 """ Members of this group with role leader should attain the is_superuser attribute """
 STAFF_LEADER_GROUPS = ["backup", "Hovedstyret", "RevyStyret"]
 """ Members of this group with role leader should attain the is_staff attribute """
