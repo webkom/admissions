c9cc3438cd6778ff27fae1fb01d559d8a8fcd785
Jonatan Norbye <jonatan@norbye.com>
2024-01-08 17:27:03 +1300

    Run cypress tests in ci

--- cypress/e2e/landing_page_spec.cy.ts
+++ cypress/e2e/landing_page_spec.cy.ts
@@ -1,12 +1,7 @@
 describe("landing page spec", () => {
-  before(() => {
-    cy.clearDb();
-  });
-
   it("successfully loads", () => {
     cy.visit("/");
     cy.contains("Opptak");
-    cy.contains("Ingen åpne opptak for øyeblikket");
     cy.contains("Logg inn");
   });
 });
