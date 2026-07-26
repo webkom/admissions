a31f74fc94da16e5817cd7e5f47fa9c0a8d86afa
Viljen Apalset Vassbø <90694640+Viljen789@users.noreply.github.com>
2026-07-26 04:13:44 +0200

    test(cypress): use deterministic application fixture

--- cypress/e2e/submit_application_spec.cy.ts
+++ cypress/e2e/submit_application_spec.cy.ts
@@ -2,14 +2,13 @@ describe("submit application spec", () => {
   it("successfully apply to one group", () => {
     cy.login("webkom");
     cy.visit("/");
-    cy.contains("Komitéopptak")
+    cy.contains("Webkomopptak – åpent")
       .parent()
       .parent()
       .parent()
       .contains("Gå til søknad")
       .click();
-    cy.contains("Velg komité").click();
-    cy.contains("Gå videre").click();
+    cy.location("pathname").should("eq", "/webkom-open/min-soknad");
     cy.contains("Skriv din søknad og send inn!").should("be.visible");
     cy.get("input[name='phoneNumber']").type("12345678");
     cy.contains("Søknadstekst").type("Hei jeg vil gjerne søke");
